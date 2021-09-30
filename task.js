e241abd920cc01274fdf50e4b69e4bb91e4bd677
Maha Khudhair <mahaalsarraj85@gmail.com>
2021-09-30 21:59:33 +0300

    JS pass task solution

--- task.js
+++ task.js
@@ -1,44 +1,41 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+//Full-stack web development - JS pass task solution
+// Bubble sort Implementation using Javascript
+var fs = require('fs');
+var arr = fs.readFileSync("./data.txt", 'utf-8');
 
-console.log("Original numbers list: ", numbers)
+// Creating the bblSort function
+function bblSort(arr){
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+// use (var) instead of (let) so the variables will be immutable
+for(var i = 0; i < arr.length; i++){
+	
+// Last i elements are already in place
+for(var j = 0; j < ( arr.length - i -1 ); j++){
+	
+	// Checking if the item at present iteration
+	// is greater than the next iteration
+	if(arr[j] > arr[j+1]){
+		
+	// If the condition is true then swap them
+	var temp = arr[j]
+	arr[j] = arr[j + 1]
+	arr[j+1] = temp
+	}
+}
 }
 
-console.log("Numbers list After sorting: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
+// Print the sorted array
+ console.log(arr);
 }
 
-console.log("Numbers list After Desc sorting: ", numbers)
+// Now pass this array to the bblSort() function
+bblSort(arr);
+
+//store it in a new file called output.txt 
+fs = require('fs');
+fs.writeFile('output.txt', arr, function (err) {
+  if (err) return console.log(err);
+ 
+});
 
 
